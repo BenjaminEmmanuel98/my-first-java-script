9c3bcd6de22a7de8c79b97c6dc8e33619403c89d
BenjaminEmmanuel98 <be7060443@gmail.com>
2024-07-11 05:06:00 -0700

    ben java first commit

---
+++ java.js
@@ -0,0 +1,54 @@
+// console.warn("Hello everyone welcome here !");
+// let Gender;
+// Gender = "female";
+// console.log(Gender);
+// const firstname = "Henry";
+// const lastname = "james";
+// const age = 30;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+
+// const firstname = "Henry";
+// const lastname = "james";
+// const age = 30;
+// const isAdmin = false;
+// const x = null;
+// const y = undefined;
+// console.log(
+//   "my name is " + firstname + lastname + "  i am" + age + " years old"
+// );
+// congatination PROCESS...
+// console.log(`my name is ${firstname} ${lastname} i am ${age} years old`);
+// console.log(firstname.toUpperCase());
+// console.log(typeof y);
+// length and split
+// const title = "welcome, ICT , laptop , router ";
+// console.log(title.length);
+// console.log(title.split(","));
+// learning of Array
+const students = ["web", "home", "kitchen", 1, 2, 3, 4, 5, 6, true];
+// to add value to the arrays
+// students.push("table", "house", "Rice", "i need foods","now")
+// students[50]="gate"
+// to make the value the first
+// students.unshift("computer")
+// pop out the ,last item
+// students.pop();
+// console.log(students);
+const person = {
+  firstname: "herny",
+  lastname: "peter",
+  score: 40.5,
+  isAdmin: false,
+  hobbies: ["cooking", "bet9ja"],
+  address: {
+    city: "owerri",
+    lga: "owerri-west",
+    postalcode: 46013,
+  },
+};
+const { firstname, lastname } = person;
+console.log(firstname);
+// for jason stringify
+// console.log(JSON.stringify(person));
